621404a6a0c5116bc147ca764ad536d8a24dc954
ifthekar <sajalcse2018@gmail.com>
2023-08-19 15:10:15 +0600

    Write a program to print your name, date of birth, and mobile number.

---
+++ problem1.c
@@ -0,0 +1,13 @@
+//1. Write a program to print your name, date of birth, and mobile number.
+
+#include<stdio.h>
+int main(){
+
+    printf("Sajal\n");
+    printf("08/06/1995\n");
+    printf("01675497324");
+
+
+
+return 0;
+}
