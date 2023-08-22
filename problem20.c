57a2ea285210e308a32e4b3eab3ec7e979235db2
ifthekar <sajalcse2018@gmail.com>
2023-08-22 16:19:01 +0600

    program to find the maximum between three numbers.

---
+++ problem20.c
@@ -0,0 +1,15 @@
+//Write a program to find the maximum between three numbers.
+#include<stdio.h>
+int main()
+    {
+  int num1,num2,num3;
+  printf("Enter number1 & number2 & number3::");
+  scanf("%d %d %d",&num1,&num2,&num3);
+  if(num1>num2 || num2>num1 && num3>num1 || num3>num2){
+   printf("%d is Maximum Number.");
+
+
+   return 0;
+
+    }
+    }
