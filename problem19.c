2f6f6d6e7df18a89ba5600cd40a86dc89a8ac575
ifthekar <sajalcse2018@gmail.com>
2023-08-22 16:13:06 +0600

    program to find the maximum between two numbers.

---
+++ problem19.c
@@ -0,0 +1,17 @@
+//Write a program to find the maximum between two numbers.
+
+
+#include<stdio.h>
+int main()
+    {
+  int num1,num2;
+  printf("Enter number1 & number2::");
+  scanf("%d %d",&num1,&num2);
+  if(num1>num2 || num2>num1){
+   printf("%d is Maximum Number.");
+
+
+   return 0;
+
+    }
+    }
