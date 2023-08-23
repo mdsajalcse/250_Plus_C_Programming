04d8959cd52b490afd00c6ad1dad0ece70f8c50d
ifthekar <sajalcse2018@gmail.com>
2023-08-23 16:36:23 +0600

    program to check two integers whether are equal or not.

---
+++ problem33.c
@@ -0,0 +1,16 @@
+//Write a program to check two integers whether are equal or not.
+
+#include<stdio.h>
+int main(){
+     int num1, num2;
+     printf("input the two integer numbers ::");
+     scanf("%d %d",&num1,&num2);
+     if(num1==num2){
+        printf("two integers are equal.");
+     }
+     else{
+          printf("two integers are not equal.");
+     }
+
+return 0;
+}
