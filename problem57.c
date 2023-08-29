9b825e8e555e23eee3d01f6ea42519d918871e7c
ifthekar <sajalcse2018@gmail.com>
2023-08-29 08:06:11 +0600

    infinite loop. An infinite loop never ends. Condition is always true

---
+++ problem57.c
@@ -0,0 +1,16 @@
+//Write an infinite loop. An infinite loop never ends. Condition is always true.
+
+#include<stdio.h>
+int main(){
+
+    for(;;){
+
+        printf("Hello World");
+
+    }
+
+
+
+
+return 0;
+}
