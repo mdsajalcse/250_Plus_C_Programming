ab80b5b9dacb5459811678c8eae5d8fd2383e1c8
ifthekar <sajalcse2018@gmail.com>
2023-09-10 15:58:22 +0600

    print all alphabets from a to z.

---
+++ problem64.c
@@ -0,0 +1,16 @@
+//Write a program to print all alphabets from a to z.
+#include<stdio.h>
+int main(){
+
+      char ch;
+      printf("Alphabets from a to z::\n");
+
+      for(ch='a';ch<='z';ch++){
+
+        printf("%c\n",ch);
+      }
+
+
+
+return 0;
+}
